806d2a2336c61bd939247a8abdd9bb7d7373865f
Raul Vilchez <raulmicrosistemas@gmail.com>
2026-05-01 19:06:31 +0200

    [refactor] moved module function group_refs_blocks() to EOF.
    - Next step, erase it permanently.

--- processor.py
+++ processor.py
@@ -254,6 +254,30 @@ def group_lines_into_raw_blocks(lines, seek_refs=False):
 
   return raw_blocks
 
+def filter_and_format_blocks(raw_blocks, doc_titles):
+  """
+  Limpia, filtra y añade la directiva rST (TOC).
+  """
+  from cleaner import clean_line_content
+
+  blocks = []
+  # Subimos la directiva TOC, al almacen de bloques
+  blocks.extend(get_toc_directive())
+
+  for rb in raw_blocks:
+    # Limpiamos lo que no pudo Pandoc
+    clean_text = clean_line_content(rb)
+
+    # comprueba si es el indice .md
+    if is_legacy_toc(clean_text, doc_titles):
+      continue
+
+    # Despues de limpiar, guardamos si hay contenido
+    if clean_text:
+      blocks.append(clean_text)
+
+  return blocks
+
 # def group_refs_blocks(lines):
 #     """
 #     Maquina de estado para capturar bloques de referencias.
@@ -303,27 +327,3 @@ def group_lines_into_raw_blocks(lines, seek_refs=False):
 #     return all_ref_blocks
 
 # El filtro de bloques
-def filter_and_format_blocks(raw_blocks, doc_titles):
-  """
-  Limpia, filtra y añade la directiva rST (TOC).
-  """
-  from cleaner import clean_line_content
-
-  blocks = []
-  # Subimos la directiva TOC, al almacen de bloques
-  blocks.extend(get_toc_directive())
-
-  for rb in raw_blocks:
-    # Limpiamos lo que no pudo Pandoc
-    clean_text = clean_line_content(rb)
-
-    # comprueba si es el indice .md
-    if is_legacy_toc(clean_text, doc_titles):
-      continue
-
-    # Despues de limpiar, guardamos si hay contenido
-    if clean_text:
-      blocks.append(clean_text)
-
-  return blocks
-
