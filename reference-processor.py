d0dc174581e63f8e632857fce97ff10a8dba3f61
Raul Vilchez <raulmicrosistemas@gmail.com>
2026-05-01 16:45:59 +0200

    [refactor] Ensure context lines end with a newline character
    - Append a trailing newline to the final element in context_lines
    to maintain file formatting.

--- reference-processor.py
+++ reference-processor.py
@@ -68,6 +68,10 @@ def extract_references_context(context_input):
     if striped_line not in context_lines:
       context_lines.append(striped_line)
 
+  # Introducimos un salto de linea en cada nueva addicion
+  if context_lines = line.rstrip():
+    context_lines[-1] += '\n'
+
 
   return context_lines
 
