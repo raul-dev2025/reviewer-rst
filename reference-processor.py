4845ce9a99b6f44fba8516d79e0418b2e6f741f0
Raul Vilchez <raulmicrosistemas@gmail.com>
2026-05-02 12:02:45 +0200

    feat(reference-processor): fix the correct way to call the second argument of re.finditer()

--- reference-processor.py
+++ reference-processor.py
@@ -63,7 +63,7 @@ def extract_references_context(context_input):
   for line in context_input:
       # Habra que iterar la busqueda de la regex
     full_text = ''.join(context_input)
-    matches = list(re.finditer(r'(?:^|\s+)\[#?[a-zA-Z0-9]+\]'), full_text)
+    matches = list(re.finditer(r'(?:^|\s+)\[#?[a-zA-Z0-9]+\]', full_text))
 
     # buscamos un  mecanismo que guarde el contexto
     preceding_text = line[:start_idx].strip()
