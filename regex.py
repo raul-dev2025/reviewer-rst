1a3f97425483d49e2e4e7704226e26925955a302
Raul Vilchez <raulmicrosistemas@gmail.com>
2026-05-05 18:42:57 +0200

    feat(regex): add patterns for RST cleaning and validation
    - Adds PANDOC_LINK_PATTERN for extracting and processing Pandoc links.
    - Defines PANDOC_ANCHOR_PATTERN for removing Pandoc anchors.
    - Includes RST_HEADER_PATTERN to process reStructuredText metadata headers.
    - Adds CONTENTS_DIRECTIVE_PATTERN to filter out obsolete document directives.

--- regex.py
+++ regex.py
@@ -6,6 +6,12 @@ REF_BRACKET = r'\[#?[a-zA-Z0-9]+\]'
 REF_PANDOC = r'`[^`]+<#?[a-zA-Z0-9]+>`__'
 REF_DECLARATION = r'\.\.\s+\[#?[a-zA-Z0-9]+\]'
 
+# Patrones adicionales para limpieza y validación RST
+PANDOC_LINK_PATTERN = r'`([^<]+)\s*<[^>]+>`__'
+PANDOC_ANCHOR_PATTERN = r'\s*<#?[a-zA-Z0-9]+>`__'
+RST_HEADER_PATTERN = r'\.\. _[a-zA-Z0-9_-]+:\n*'
+CONTENTS_DIRECTIVE_PATTERN = r'^\.\. contents::.*?\n(\s+:[a-z]+:.*?\n)*'
+
 # Expresión regular combinada usando alternancia
 # Utiliza grupos de no captura para evaluar cualquiera de los tres formatos
 reference_pattern = re.compile(
